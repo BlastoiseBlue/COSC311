398df4785c0ae2d99fb3b86d7750cccdbbd0834d
Unknown <emmet@stanevich.com>
2019-01-09 15:49:14 -0500

    Created emmetSquares

---
+++ emmetSquares/src/Driver.java
@@ -0,0 +1,12 @@
+/**
+ * @author Emmet Stanevich
+ * Outputs the first 50 squares. This class is the driver for the lab.
+ */
+public class Driver {
+    /**
+     * @param args References command line arguments
+     */
+    public static void main(String[] args){
+        Printer.squares();
+    }
+}
