fb534bdb24dc49724dc4e899f44563cf3951b886
Unknown <emmet@stanevich.com>
2019-01-14 16:30:14 -0500

    Created method shells
    Extremely barebones

--- emmetFirstGeneric/src/OrderedCollection.java
+++ emmetFirstGeneric/src/OrderedCollection.java
@@ -1,11 +1,31 @@
 /**
  * @author Emmet Stanevich
  */
-public class OrderedCollection<AnyType> {
+public class OrderedCollection <AnyType extends Comparable<? super AnyType>> {
+	//public static <AnyType >
+	public OrderedCollection(){
+		//memory=new AnyType[];
+	}
 	public boolean isEmpty(){
-
+		if(memory==null)return true;
+		else return false;
 	}
 	public void makeEmpty(){
-
+		memory=null;
+	}
+	public void insert(){
+		//if(isEmpty())memory=new AnyType[1];
+		AnyType[] mem1=new Anytype[memory.length+1];
+		for(int i=0;i<memory.length;i++)mem1[i]=memory[i];
+		
+	}
+	public void remove(AnyType x){
+	
+	}
+	AnyType[] memory;
+	AnyType findMax(AnyType [] arr){
+		if(!isEmpty()){
+			//for()
+		}
 	}
 }
