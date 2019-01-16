3b2ad5a141adb6f8555329a2831eb6428423f52c
Unknown <emmet@stanevich.com>
2019-01-16 13:46:24 -0500

    Added .gitignore
    Also changed formatting on Printer.java

--- emmetSquares/src/Printer.java
+++ emmetSquares/src/Printer.java
@@ -3,20 +3,20 @@
  * Prints out the first 50 squares and their sum and average
  */
 public class Printer {
-
-    private static final int NUM_SQUARES = 50;
-
-    public static void main(String[] args){
-        squares();
-    }
-    public static void squares(){
-        int sum=0;
-        for(int i = 1; i<= NUM_SQUARES; i++){
-            sum+=(i*i);
-            System.out.print(i*i);
-            if(i<NUM_SQUARES)System.out.print(", ");
-        }
-        double average=sum/(double)NUM_SQUARES;
-        System.out.println("\nSum: "+sum+"\nAverage: "+average);
-    }
+	
+	private static final int NUM_SQUARES = 50;
+	
+	public static void main(String[] args){
+		squares();
+	}
+	public static void squares(){
+		int sum=0;
+		for(int i = 1; i<= NUM_SQUARES; i++){
+			sum+=(i*i);
+			System.out.print(i*i);
+			if(i<NUM_SQUARES)System.out.print(", ");
+		}
+		double average=sum/(double)NUM_SQUARES;
+		System.out.println("\nSum: "+sum+"\nAverage: "+average);
+	}
 }
