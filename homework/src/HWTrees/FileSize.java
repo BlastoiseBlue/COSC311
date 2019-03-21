3bec2d58eb776f0dfd3705aa815cce601734ddc0
Unknown <emmet@stanevich.com>
2019-03-21 12:21:31 -0400

    Began working on FileSize.java
    Still unfinished, but I'm hoping to make some progress on this

--- homework/src/HWTrees/FileSize.java
+++ homework/src/HWTrees/FileSize.java
@@ -1,8 +1,26 @@
 package HWTrees;
-
+import java.io.File;
 /**
  * @author Emmet Stanevich
+ * Created on 3/20/19
  */
-public class FileSize {
+public class FileSize extends File {
 	
+	public FileSize(String pathname) {
+		super(pathname);
+	}
+	public static void printName(int depth){
+		for(int i=0;i<depth;i++)System.out.print("\t");
+		System.out.print();
+	}
+	public void listAll(){
+		listAll(0);
+	}
+	private void listAll(int depth){
+		printName(depth);
+		if(isDirectory()) for (File c:
+		                       ) {
+			
+		}
+	}
 }
