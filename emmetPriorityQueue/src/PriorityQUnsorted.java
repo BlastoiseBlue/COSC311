effbdfcd9681a927b79dec872fbcd3aefebd76c6
Unknown <emmet@stanevich.com>
2019-04-10 18:01:57 -0400

    Implemented PriorityQUnsorted

--- emmetPriorityQueue/src/PriorityQUnsorted.java
+++ emmetPriorityQueue/src/PriorityQUnsorted.java
@@ -1,7 +1,59 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 /**
  * @author Emmet Stanevich
+ * Created on 4/10/19
+ * This program will accept comparables and can, without sorting them, find and remove the smallest one
  */
-public class PriorityQUnsorted {
+public class PriorityQUnsorted <T extends Comparable<? super T>>{
+	ArrayList<T> element=new ArrayList<>();
+	public boolean isEmpty(){
+		return element.isEmpty();
+	}
+	public void add(T value){
+		element.add(value);
+//		for(int i=element.size()-1;i>0;i--){
+//			if(element.get(i).compareTo(element.get(i - 1)) < 0)bubbleUp(i);
+//		}
+	}
+	private void bubbleUp(int i){
+		T placeHolder=element.get(i-1);
+		element.set(i-1,element.get(i));
+		element.set(i,placeHolder);
+	}
+	public T deleteMin(){
+		return remove();
+	}
+	public T remove(){
+		T placeHolder=findMin();
+		//for(int i=0;i<element.size()-1;i++)element.set(i,element.get(i+1));
+//		for(int i=0;i<element.size();i++){
+//			if(element.get(i).compareTo(element.get(minIndex))<0)minIndex=i;
+//		}
+		element.remove(minIndex());
+		return placeHolder;
+	}
+	private int minIndex(){
+		int m=0;
+		for(int i=0;i<element.size();i++){
+			if(element.get(i).compareTo(element.get(m))<0)m=i;
+		}
+		return m;
+	}
+	public T findMin(){
+//		return element.get(0);
+//		int minIndex=0;
+//		for(int i=0;i<element.size();i++){
+//			if(element.get(i).compareTo(element.get(minIndex))<0)minIndex=i;
+//		}
+		return element.get(minIndex());
+	}
+	public String toString(){
+		String s="";
+		for (T value:element) {
+			s+=(value+", ");
+		}
+		s=s.replaceAll(", $","");
+		return s;
+	}
 }
--- emmetPriorityQueue/src/PriorityQUnsortedDemo.java
+++ emmetPriorityQueue/src/PriorityQUnsortedDemo.java
@@ -3,47 +3,47 @@
  */
 public class PriorityQUnsortedDemo
 {
-//	public static <AnyType extends Comparable<? super AnyType>>
-//	void dumpPQ(String msg, PriorityQUnsorted<AnyType> pq)
-//	{
-//		System.out.println( msg + ":" );
-//		while( !pq.isEmpty( ) )
-//			System.out.println( pq.remove( ) );
-//	}
-//
-//	// Do some inserts and removes (done in dumpPQ).
-//	public static void main( String [ ] args )
-//	{
-//		PriorityQUnsorted<Integer> minPQ = new PriorityQUnsorted<Integer>( );
-//
-//		minPQ.add( 4 );
-//		minPQ.add( 3 );
-//		minPQ.add( 5 );
-//
-//		Integer result = minPQ.findMin();
-//		System.out.println("Smallest value in priorityQ<> should be 3 and is "+result);
-//
-//		minPQ.deleteMin( );
-//		result = minPQ.findMin();
-//		System.out.println("Smallest value in priorityQ<> should be 4 and is "+result);
-//
-//		System.out.println( minPQ );
-//		dumpPQ( "minPQ", minPQ );
-//
-//		PriorityQUnsorted<String> minPQStr = new PriorityQUnsorted<String>( );
-//
-//		minPQStr.add( "four" );
-//		minPQStr.add( "three" );
-//		minPQStr.add( "five" );
-//
-//		String myResult = minPQStr.findMin();
-//		System.out.println("Smallest value in priorityQ<> should be five and is "+myResult);
-//
-//		minPQStr.deleteMin( );
-//		myResult = minPQStr.findMin();
-//		System.out.println("Smallest value in priorityQ<> should be four and is "+myResult);
-//
-//		System.out.println( minPQStr );
-//		dumpPQ( "minPQ", minPQStr );
-//	}
+	public static <AnyType extends Comparable<? super AnyType>>
+	void dumpPQ(String msg, PriorityQUnsorted<AnyType> pq)
+	{
+		System.out.println( msg + ":" );
+		while( !pq.isEmpty( ) )
+			System.out.println( pq.remove( ) );
+	}
+
+	// Do some inserts and removes (done in dumpPQ).
+	public static void main( String [ ] args )
+	{
+		PriorityQUnsorted<Integer> minPQ = new PriorityQUnsorted<Integer>( );
+
+		minPQ.add( 4 );
+		minPQ.add( 3 );
+		minPQ.add( 5 );
+
+		Integer result = minPQ.findMin();
+		System.out.println("Smallest value in priorityQ<> should be 3 and is "+result);
+
+		minPQ.deleteMin( );
+		result = minPQ.findMin();
+		System.out.println("Smallest value in priorityQ<> should be 4 and is "+result);
+
+		System.out.println( minPQ );
+		dumpPQ( "minPQ", minPQ );
+
+		PriorityQUnsorted<String> minPQStr = new PriorityQUnsorted<String>( );
+
+		minPQStr.add( "four" );
+		minPQStr.add( "three" );
+		minPQStr.add( "five" );
+
+		String myResult = minPQStr.findMin();
+		System.out.println("Smallest value in priorityQ<> should be five and is "+myResult);
+
+		minPQStr.deleteMin( );
+		myResult = minPQStr.findMin();
+		System.out.println("Smallest value in priorityQ<> should be four and is "+myResult);
+
+		System.out.println( minPQStr );
+		dumpPQ( "minPQ", minPQStr );
+	}
 }
